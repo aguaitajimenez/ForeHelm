ffaef7e7e22e136636256801e4fd095ac7b1a8ef
aguaitajimenez <alix0405@gmail.com>
2024-11-19 15:25:22 -0600

    Commit can capture images, produce inferences with standard YOLOv11n and train a dataset

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import cv2
-cap = cv2.VideoCapture(0)
-while True:
-   status, photo = cap.read()
-   cv2.imshow("Streaming Video", photo)
-   if cv2.waitKey(10) == 13:
-      break
-cv2.destroyAllWindows()
+
+from ultralytics import YOLO
+from IPython.display import display, Image
+
+def main():
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = cap.read()
+        print(frame.shape)  
+        cv2.imshow("yolov8", frame)
+
+        if(cv2.waitKey(30) == 27):
+            break
+        
+
+if __name__ == "__main__":
+   main()
